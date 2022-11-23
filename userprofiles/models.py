d4b867b2e85577644984d7eac3e9d2a7f884cf85
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-23 13:09:01 +0530

    Profile model configured

--- userprofiles/models.py
+++ userprofiles/models.py
@@ -8,7 +8,7 @@ from django.conf import settings
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
-    def create_user(self, email, name, username, profilepicture, password=None):
+    def create_user(self, email, name, username, password=None):
         """Create a new user profile"""
         if not email:
             raise ValueError('Users must have an email address')
@@ -17,16 +17,16 @@ class UserProfileManager(BaseUserManager):
             raise ValueError('Users must have an username')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name, username=username, profilepicture=profilepicture)
+        user = self.model(email=email, name=name, username=username)
 
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, name, username, profilepicture, password):
+    def create_superuser(self, email, name, username, password):
         """Create and save a new superuser with given details"""
-        user = self.create_user(email, name, username, profilepicture, password)
+        user = self.create_user(email, name, username, password)
 
         user.is_superuser = True
         user.is_staff = True
@@ -34,12 +34,11 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-class UserProfile(AbstractBaseUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     username = models.CharField(max_length=255, primary_key=True)
-    profilepicture = models.ImageField(upload_to='profilepictures/')
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
@@ -59,3 +58,8 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """Return string representation of user"""
         return self.username
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profilepicture = models.ImageField(upload_to='profilepictures/')
+    bio = models.CharField(max_length=500, null=True)
