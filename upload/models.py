b8ce50ed3951aaf7c9baa4aaf0bdd6e918da22f3
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-23 13:59:45 +0530

    File model configured

--- upload/models.py
+++ upload/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import os
+from userprofiles.models import User
 
 # Create your models here.
+def create_path(instance, filename):
+    return os.path.join(
+        instance.owned_by,
+        filename
+    )
+
+class File(models.Model):
+    filename = models.CharField(max_length=100)
+    file = models.FileField(upload_to=create_path)
+    owned_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    file_id = models.CharField(max_length=100, primary_key=True)
+    urltoken = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
