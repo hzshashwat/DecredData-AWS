0a4f0b407e1995b77351d8b88d1f913c50eef55f
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-24 22:53:01 +0530

    Added requirements.txt file

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn decreddata.wsgi.py
--- decreddata/settings.py
+++ decreddata/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--)z0wuya4483e2ed!dc(4nh5xle1bwrqds%9qk(+&bg^wj#5&y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
